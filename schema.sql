25b02fa55cb69ba9027693176f56ed3d2ddbeb85
atok624 <84607674+atok624@users.noreply.github.com>
2023-05-04 13:08:29 +0000

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -8,3 +8,23 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(255);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+ALTER TABLE animals
+  ADD COLUMN species_id INTEGER,
+  ADD COLUMN owner_id INTEGER,
+  DROP COLUMN species,
+  ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id),
+  ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
